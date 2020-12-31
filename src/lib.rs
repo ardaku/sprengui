6fa007abbbdbfb5d89bcebbf5a2db1e0dc338b1a
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-12-31 02:22:10 -0600

    Initialize repository.

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+// Sprengui
+// Copyright © 2020 Jeron Aldaron Lau.
+//
+// Licensed under any of:
+//  - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+//  - MIT License (https://mit-license.org/)
+//  - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
+// At your choosing (See accompanying files LICENSE_APACHE_2_0.txt,
+// LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).
+
+/// Graphical window.
+pub struct Window {
+    title: String
+}
+
+impl Window {
+    pub fn new<T: ToString>(title: T) -> Self {
+        Window {
+            title: title.to_string()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
