8652b5aba2e96a38af17c42ec90d364bef920d7f
adaburton <94191498+adaburton@users.noreply.github.com>
2022-07-13 15:26:50 -0700

    Implement `Notifier` for `Window` & Add `Action` Enum (#11)
    * Add files via upload
    
    * Delete Cargo.toml
    
    * Add files via upload
    
    * Add files via upload
    
    * Update Cargo.toml
    
    * Add files via upload
    
    * Add files via upload
    
    fleshed out the Actions enum and started related structs and enums
    
    * Update lib.rs
    
    * Add files via upload
    
    * Update lib.rs
    
    * Add files via upload
    
    * Delete Cargo.toml
    
    * Delete lib.rs
    
    * Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@
 // At your choosing (See accompanying files LICENSE_APACHE_2_0.txt,
 // LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).
 
+use pasts::{Notifier, prelude::*};
+
 /// Text formatting
 pub enum Format {
     /// Paragraph
@@ -76,6 +78,76 @@ enum Icon {
     Wand,
     /// Pencil icon
     Pencil,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+/// ID for a MenuAction
+pub struct MenuAction {
+    // what is happening
+    action_id: usize,
+    // where
+    menu_id: usize,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+/// ID for a WindowAction
+pub struct WindowAction {
+    // what is happening
+    action_id: usize,
+    // where
+    window_id: usize,
+}
+
+
+#[derive(Clone, PartialEq, Eq)]
+/// ID for a search action
+pub struct SearchAction {
+    // what are we searching for?
+    query: String,
+    // where are we looking?
+    db_id: usize,
+}
+
+/// ID for button action
+pub struct ButtonAction {
+    // what is happening?
+    action_id: usize,
+    // where?
+    entry_id: usize,
+}
+
+pub struct FieldAction;
+
+pub struct SliderAction;
+
+/// Actions that produce data
+pub enum EntryAction {
+    Button(ButtonAction),
+    Field(FieldAction),
+    Slider(SliderAction),
+}
+
+/// Actions performed with the tab key
+pub enum TabAction {
+    SelectTab,
+    MoveTab,
+    ReverseTab,
+}
+
+/// Actions
+///
+/// Events that are returned back to the user.
+pub enum Action {
+    /// Interaction with a menu
+    Menu(MenuAction),
+    /// Interaction with a window
+    Window(WindowAction),
+    /// Search action
+    Search(SearchAction),
+    /// Input interactions
+    Entry(EntryAction),
+    /// Tab functionality
+    Tab(TabAction)
 }
 
 struct MenuItem {
@@ -188,6 +260,15 @@ impl Window {
         todo!()
     }
 }
+
+impl Notifier for Window {
+    type Event = Action;
+
+    fn poll_next(self: Pin<&mut Self>, _cx: &mut Exec<'_>) -> Poll<Self::Event> {
+       Poll::Pending
+    } 
+}
+
 
 /// A GUI page
 pub struct Page {
