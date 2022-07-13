2c12369ceb3f9d64d778b556304ee4fbe50bdf73
adaburton <94191498+adaburton@users.noreply.github.com>
2022-07-13 15:15:55 -0700

    Add files via upload
    Added free function bezier to calculate the bezier coefficient for a given point in an animations cycle

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@
 // At your choosing (See accompanying files LICENSE_APACHE_2_0.txt,
 // LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).
 
+use pasts::{Notifier, prelude::*};
+
 /// Text formatting
 pub enum Format {
     /// Paragraph
@@ -76,6 +78,76 @@ enum Icon {
     Wand,
     /// Pencil icon
     Pencil,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+/// ID for a MenuAction
+pub struct MenuAction {
+    // what is happening
+    action_id: usize,
+    // where
+    menu_id: usize,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+/// ID for a WindowAction
+pub struct WindowAction {
+    // what is happening
+    action_id: usize,
+    // where
+    window_id: usize,
+}
+
+
+#[derive(Clone, PartialEq, Eq)]
+/// ID for a search action
+pub struct SearchAction {
+    // what are we searching for?
+    query: String,
+    // where are we looking?
+    db_id: usize,
+}
+
+/// ID for button action
+pub struct ButtonAction {
+    // what is happening?
+    action_id: usize,
+    // where?
+    entry_id: usize,
+}
+
+pub struct FieldAction;
+
+pub struct SliderAction;
+
+/// Actions that produce data
+pub enum EntryAction {
+    Button(ButtonAction),
+    Field(FieldAction),
+    Slider(SliderAction),
+}
+
+/// Actions performed with the tab key
+pub enum TabAction {
+    SelectTab,
+    MoveTab,
+    ReverseTab,
+}
+
+/// Actions
+///
+/// Events that are returned back to the user.
+pub enum Action {
+    /// Interaction with a menu
+    Menu(MenuAction),
+    /// Interaction with a window
+    Window(WindowAction),
+    /// Search action
+    Search(SearchAction),
+    /// Input interactions
+    Entry(EntryAction),
+    /// Tab functionality
+    Tab(TabAction)
 }
 
 struct MenuItem {
@@ -189,6 +261,15 @@ impl Window {
     }
 }
 
+impl Notifier for Window {
+    type Event = Action;
+
+    fn poll_next(self: Pin<&mut Self>, _cx: &mut Exec<'_>) -> Poll<Self::Event> {
+       Poll::Pending
+    } 
+}
+
+
 /// A GUI page
 pub struct Page {
     widgets: Vec<Widget>,
@@ -216,6 +297,24 @@ impl Page {
     pub fn push(&mut self, widget: Widget) {
         self.widgets.push(widget);
     }
+}
+
+/// Bezier
+///
+/// param t, float between 0 and 1 that describes
+///     at which point in the animation to get the
+///     bezier coefficient
+/// 
+/// return the bezier coefficient for the given
+///     point in the animation, or none if given
+///     an invalid time.
+fn bezier(t: f32) -> Option<f32> {
+    // make sure input is valid
+    if t > 1.0 || t < 0.0 {
+        return None;
+    }
+
+    Some(3.0 * (1.0 - t) * (t * t))
 }
 
 #[cfg(test)]
